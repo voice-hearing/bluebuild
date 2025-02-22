8b83edb366b1881fb86e5f14af27026a452c9d0d
Ben Homan <ben@benhoman.com>
2025-02-22 14:40:32 -0500

    updates

--- files/scripts/ensureautoupdates.sh
+++ files/scripts/ensureautoupdates.sh
@@ -6,5 +6,6 @@ set -oue pipefail
 systemctl enable rpm-ostreed-automatic.timer
 systemctl enable podman-auto-update.timer
 systemctl --global enable podman-auto-update.timer
-systemctl --global enable flatpak-user-update.timer
-systemctl enable flatpak-system-update.timer
+# systemctl --global enable flatpak-user-update.timer
+# systemctl enable flatpak-system-update.timer
+
